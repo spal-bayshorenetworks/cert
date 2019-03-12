70e02c4cf6a585f48b2f79ad244de7f465522555
spal-bayshorenetworks <19271770+spal-bayshorenetworks@users.noreply.github.com>
2019-03-12 10:10:16 -0400

    Create procmon.sh

---
+++ scripts/procmon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IFS=$"\n"
+
+old_process=$(ps -eo command)
+while true; do
+    new_process=$(ps -eo command)
+	diff <(echo "$old_process") <(echo "$new_proces") | grep [\>\<]
+	sleep 1
+	old_process=$new_process
+done
